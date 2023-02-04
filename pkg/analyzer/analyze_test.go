5218b2657860eb38d0676ddd5a1dd856fd8bfa70
Maksim Meshkov <subtle.byte.dev@gmail.com>
2023-02-04 16:28:50 +0100

    Add helper to run from code (#1)
    Add helper to run from code

---
+++ pkg/analyzer/analyze_test.go
@@ -0,0 +1,42 @@
+package analyzer
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAnalyze(t *testing.T) {
+	type A struct {
+		Name string
+		Age  int
+	}
+	_ = A{
+		Name: "John",
+		//allfields:lint
+	}
+	_ = A{
+		Name: "John",
+		Age:  10,
+		//allfields
+	}
+	_ = A{
+		//allfields
+	}
+	_ = A{}
+	errs := []string(nil)
+	Analyze(AnalyzeConfig{
+		PackagesPattern: ".",
+		ReportErr: func(message string) {
+			errs = append(errs, message)
+		},
+		Tests: true,
+	})
+	for _, err := range errs {
+		t.Log(err)
+	}
+	require.Len(t, errs, 2)
+	assert.Contains(t, errs[0], "analyzer/analyze_test.go:15:6: field Age is not set")
+	assert.Contains(t, errs[1], "analyzer/analyze_test.go:24:6: fields Name, Age are not set")
+}
